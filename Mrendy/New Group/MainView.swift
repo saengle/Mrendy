b567988a402d41b5e7dcb4c93c9b0e6cd1d2151d
saengle <zohv817@gmail.com>
2024-06-10 20:15:32 +0900

    Initial Commit

---
+++ Mrendy/New Group/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Mrendy
+//
+//  Created by 쌩 on 6/10/24.
+//
+
+import UIKit
+
+class MainView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .cyan
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Mrendy/New Group/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Mrendy
+//
+//  Created by 쌩 on 6/10/24.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {
+    
+    let mainView = MainView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension MainViewController {
+    
+}
