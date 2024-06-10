019d5248436d85d9b85ea87186da409b21c7870b
saengle <zohv817@gmail.com>
2024-06-11 00:06:01 +0900

    [Feat] Common Extension
    Cell Identifier
    ViewController Identifier

---
+++ Mrendy/Extension/Common+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Common+Extension.swift
+//  Mrendy
+//
+//  Created by 쌩 on 6/11/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static let identifier = String(describing: UITableViewCell.self)
+}
+
+extension UIViewController {
+    static let identifier = String(describing: UIViewController.self)
+}
