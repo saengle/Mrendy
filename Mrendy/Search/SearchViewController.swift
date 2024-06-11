7ead533f64ba25be603b282e3e56b58752c75d7f
saengle <zohv817@gmail.com>
2024-06-11 19:40:24 +0900

    [Feat] Keyboard Dismiss

--- Mrendy/Search/SearchViewController.swift
+++ Mrendy/Search/SearchViewController.swift
@@ -22,19 +22,14 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        apiManager.callRequestSearch(query: "king") { result in
-            switch result {
-            case .success(let trendy):
-                self.searchedTrendyList = [trendy]
-                self.searchView.searchedCollectionView.reloadData()
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
         
+        //collection view, cell 연결 및 등록
         searchView.searchedCollectionView.dataSource = self
         searchView.searchedCollectionView.delegate = self
         searchView.searchedCollectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.identifier)
+        
+        searchView.searchBar.delegate = self
+        hideKeyboard()
     }
     
 }
@@ -52,8 +47,33 @@ extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSo
         
         let data = searchedTrendyList.first?.results?[indexPath.row]
         
-        cell.configureCell(url: data?.posterPath ?? "")
+        cell.configureCell(url: data?.posterPath ?? data?.backdropPath ?? "")
         
         return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.apiManager.callRequestSearch(query: searchBar.text!) { result in
+            switch result {
+            case .success(let trendy):
+                self.searchedTrendyList = [trendy]
+                self.searchView.searchedCollectionView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        dismissKeyboard()
+    }
+    
+    func hideKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
