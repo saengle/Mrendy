fe23dc15de94f902c66f99d12c22b3f45b2d5970
saengle <zohv817@gmail.com>
2024-06-26 00:33:21 +0900

    [Feat] DetailScreen TableView Register each Cell

--- Mrendy/DetailScreen/DetailViewController.swift
+++ Mrendy/DetailScreen/DetailViewController.swift
@@ -26,13 +26,25 @@ extension DetailViewController {
         self.detailView.mainTableView.delegate = self
         self.detailView.mainTableView.dataSource = self
         self.detailView.mainTableView.rowHeight = UITableView.automaticDimension
+        
+        
+       
+//        self.detailView.mainTableView.cell
     }
 }
 
 extension DetailViewController: UITableViewDataSource, UITableViewDelegate {
     // MARK:  Number of Rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 4
+        case 2...3:
+            return 1
+        default: return 0
+        }
     }
     // MARK: Number of Sections
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -40,14 +52,38 @@ extension DetailViewController: UITableViewDataSource, UITableViewDelegate {
     }
     // MARK:  ConfigureCell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: <#T##IndexPath#>)
-            let cell = UITableViewCell()
-        cell.backgroundColor = .brown
-        return cell
+     
+        if indexPath.section == 0 {
+            self.detailView.mainTableView.register(OverViewCell.self, forCellReuseIdentifier: OverViewCell.id)
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: OverViewCell.id, for: indexPath)
+                    as? OverViewCell else { return UITableViewCell()}
+            print(indexPath.section)
+            
+            return cell
+        } else if indexPath.section == 1 {
+            self.detailView.mainTableView.register(CastCell.self, forCellReuseIdentifier: CastCell.id)
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CastCell.id, for: indexPath)
+                    as? CastCell else { return UITableViewCell()}
+            print(indexPath.section)
+            return cell
+        } else {
+            self.detailView.mainTableView.register(VideoListCell.self, forCellReuseIdentifier: VideoListCell.id)
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: VideoListCell.id, for: indexPath)
+                    as? VideoListCell else { return UITableViewCell()}
+            print(indexPath.section)
+            return cell
+        }
     }
     // MARK:  Header Title
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "안녕 디지몬"
+        switch section {
+        case 0:
+            return "OvewView"
+        case 1:
+            return "Cast"
+        case 2...3:
+            return "울랄라"
+        default: return ""
+        }
     }
-   
 }
